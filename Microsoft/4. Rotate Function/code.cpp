b2f2844c8d427c7acc38a4cd1a9497aeb5185995
Jatin Aggarwal <55448767+jatin-1305@users.noreply.github.com>
2023-01-02 17:13:59 +0530

    Update code.cpp

--- Microsoft/4. Rotate Function/code.cpp
+++ Microsoft/4. Rotate Function/code.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int cal=0;
+        for(int i=0;i<nums.size();i++) {
+            cal += (i*nums[i]);
+        }
+        int ans=cal;
+        for(int ele=nums.size()-1;ele>-1;ele--) {
+            cal = cal+sum-(nums.size()*nums[ele]);
+            ans = max(ans,cal);
+        }
+        return ans;
+    }
+};
