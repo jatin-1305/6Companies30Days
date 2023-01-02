568d6bc2ef0cfb3c7a4c4ed98678cbcc64570cb1
Jatin Aggarwal <55448767+jatin-1305@users.noreply.github.com>
2023-01-02 21:41:14 +0530

    Update code.cpp

--- Microsoft/5. Largest Divisible Subset/code.cpp
+++ Microsoft/5. Largest Divisible Subset/code.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& A) {
+      int n = A.size();
+      sort(A.begin(), A.end());
+      vector<int> dp(n, 1);
+      vector<int> previous_index(n, -1);
+      int max_ind = 0;
+      
+       for(int i=1; i<n; i++) {
+           for(int j=0; j<i; j++) {
+               if(A[i]%A[j]==0 and dp[i] < dp[j] + 1) {
+                   dp[i] = 1+dp[j];
+                   previous_index[i] = j;
+               }
+           }
+           if(dp[i] > dp[max_ind]) {
+               max_ind = i;
+           }
+       }        
+      vector<int> answer;
+      int t = max_ind;  
+      while(t >= 0) {
+          answer.push_back(A[t]);
+          t = previous_index[t];
+      }
+      return answer;  
+    }
+};
