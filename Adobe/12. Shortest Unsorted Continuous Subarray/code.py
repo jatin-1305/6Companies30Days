a1350302611e7b008e6e6f7d07ae670cb9e5aac8
Jatin Aggarwal <55448767+jatin-1305@users.noreply.github.com>
2023-01-13 17:19:16 +0530

    Create code.py

---
+++ Adobe/12. Shortest Unsorted Continuous Subarray/code.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findUnsortedSubarray(self, nums):
+        l=nums[:]
+        l.sort()
+        print(nums,l)
+        count,left,right=0,0,0
+        for i in range(len(nums)):
+            if l[i]!=nums[i]:
+                left = i
+                break
+        for i in range(len(nums)-1,-1,-1):
+            if l[i]!=nums[i]:
+                right = i
+                break
+        if left==0 and right==0:
+            return 0
+        elif left==right:
+            return 1
+        return right-left+1
