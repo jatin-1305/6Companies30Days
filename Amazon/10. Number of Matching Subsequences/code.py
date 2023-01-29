4e649f4ffc5c64db13e8d925c04efa5ca2faef4b
Jatin Aggarwal <55448767+jatin-1305@users.noreply.github.com>
2023-01-29 18:02:50 +0530

    Create code.py

---
+++ Amazon/10. Number of Matching Subsequences/code.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numMatchingSubseq(self, S: str, words: List[str]) -> int:
+        def isMatch(word, w_i, d_i):
+            if w_i == len(word): return True
+            l = dict_idxs[word[w_i]]
+            if len(l) == 0 or d_i > l[-1]: return False
+            i = l[bisect_left(l, d_i)]
+            return isMatch(word, w_i + 1, i + 1)
+
+        dict_idxs = defaultdict(list)
+        for i in range(len(S)):
+            dict_idxs[S[i]].append(i)
+        return sum(isMatch(word, 0, 0) for word in words)
